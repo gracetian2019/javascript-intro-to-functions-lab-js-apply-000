53bb4596bae310fb990ee1293e8137a814f5bf43
Grace Tian <gracetian4ever@gmail.com>
2020-03-11 14:29:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
-  if (string===string.toLowercase())
-    return "I can't hear you";
-  else if (string===string.toUpperCase())
-    return "YES INDEED!";
-  else if (string==="I love you, Grandma.")
-    return "I love you, too.";
+  if (string===string.toLowercase()) return "I can't hear you";
+  else if (string===string.toUpperCase()) return "YES INDEED!";
+  else if (string==="I love you, Grandma.") return "I love you, too.";
 }
